9d58da1b302cebfabdf0a30468a50e7a1fd66730
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2023-08-13 07:46:12 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,8 +19,8 @@ class BaseModel:
         """
         tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -32,7 +32,7 @@ class BaseModel:
 
     def save(self):
         """changes updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
